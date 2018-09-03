b96f0bba43c186e2f48b44ba79033490f5a17988
Iovany Giron Moreno <iovany.giron@mobiik.com>
2018-09-03 11:23:49 -0500

    Agregar archivos de proyecto.

---
+++ Git Visual proyecto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Visual_proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
